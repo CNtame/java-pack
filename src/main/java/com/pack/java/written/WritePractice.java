da0fa161172513c4d012a31b7ace33f71fba9c8a
tame <aberthai@qq.com>
2021-03-19 21:38:00 +0800

    面试需要注意点

---
+++ src/main/java/com/pack/java/written/WritePractice.java
@@ -0,0 +1,23 @@
+package com.pack.java.written;
+
+import java.util.*;
+
+
+/**
+ * @author liu
+ * @version 1.0
+ * @description
+ * @createDate 2021/3/19
+ */
+public class WritePractice {
+
+    public static void main(String[] args) {
+
+        //输入 接收
+        Scanner s=new Scanner(System.in);
+
+
+        //输出格式
+
+    }
+}
